d39d32d687bff4f6cefc10344c29240c59188e0b
marcondesfr <marcondessam@gmail.com>
2021-09-07 15:59:16 -0400

    projectFinish

--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using Composition.Entities.Enums;
+using Composition.Entities;
 
 namespace Composition
 {
@@ -6,7 +8,42 @@ namespace Composition
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            System.Console.Write("Enter department's name: ");
+            string deptName = Console.ReadLine();
+            System.Console.WriteLine("Enter worker data: ");
+            System.Console.Write("Name: ");
+            string name = Console.ReadLine();
+            System.Console.Write("Level (Junior/MidLevel/Senior): ");
+            WorkerLevel level = Enum.Parse<WorkerLevel>(Console.ReadLine());
+            System.Console.Write("Base salary: ");
+            double basesalary = double.Parse(Console.ReadLine());
+
+            Department dept = new Department(deptName);
+            Worker worker = new Worker(name, level, basesalary,dept);
+
+            System.Console.Write("How many contracts to this worker? ");
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i < n; i++) {
+                System.Console.WriteLine($"Enter #{i} contract data:");
+                System.Console.Write("Date (DD/MM/YYYY): ");
+                DateTime date = DateTime.Parse(Console.ReadLine());
+                System.Console.Write("Value per hour: ");
+                double valuePerHour = double.Parse(Console.ReadLine());
+                System.Console.Write("Duration (hours): ");
+                int hours = int.Parse(Console.ReadLine());
+                HourContract contract = new HourContract(date, valuePerHour, hours);
+                worker.AddContract(contract);
+            }
+            System.Console.WriteLine();
+            System.Console.Write("Enter month and year to calculate income (MM/YYY): ");
+            string monthAndYear = Console.ReadLine();
+            int month = int.Parse(monthAndYear.Substring(0,2));
+            int year = int.Parse(monthAndYear.Substring(3));
+
+            System.Console.WriteLine("Name " + worker.Name);
+            System.Console.WriteLine("Department: " + worker.Department.Name);
+            System.Console.WriteLine("Income for " + monthAndYear + ": " + worker.Income(year, month).ToString("F2"));
         }
     }
 }
